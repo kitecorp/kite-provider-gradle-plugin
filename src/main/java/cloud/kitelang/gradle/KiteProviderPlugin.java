d65a4b40250d8aadfdeb255063c261cc7654b8f4
Tudor Pop <tudor14pop@gmail.com>
2025-12-31 03:29:19 +0200

    chore: Bump version to 0.2.0 with SDK version update

--- src/main/java/cloud/kitelang/gradle/KiteProviderPlugin.java
+++ src/main/java/cloud/kitelang/gradle/KiteProviderPlugin.java
@@ -55,7 +55,7 @@ public class KiteProviderPlugin implements Plugin<Project> {
 
         // Set defaults
         extension.getProtocolVersion().convention(1);
-        extension.getSdkVersion().convention("0.1.0");
+        extension.getSdkVersion().convention("0.2.0");
         extension.getDocsEnabled().convention(true);
         extension.getDocsFormats().convention("html,markdown");
         extension.getDocsOutputDir().convention("build/docs/provider");
